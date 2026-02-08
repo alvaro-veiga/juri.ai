ce0ce838e6ee173a0886f107e8749934ca5c795f
Álvaro Veiga <alvarojoao17@gmail.com>
2026-02-08 09:59:22 -0300

    Criando front end do chat

--- ai/views.py
+++ ai/views.py
@@ -1,3 +1,9 @@
+from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
+from usuarios.models import Cliente
 
-# Create your views here.
+@csrf_exempt
+def chat(request, id):
+    cliente = Cliente.objects.get(id=id)
+    if request.method == 'GET':
+        return render(request, 'chat.html', {'cliente': cliente})
--- usuarios/signals.py
+++ usuarios/signals.py
@@ -2,11 +2,12 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import Documentos
 from ai.tasks import ocr_and_markdown_file, rag_documentos
-from django_q.tasks import async_task
+from django_q.tasks import Chain
 
 @receiver(post_save, sender=Documentos)
 def save_documents(sender, instance, created, **kwargs):
-    
-    async_task(ocr_and_markdown_file, instance.id)
-
-    rag_documentos(instance.id)
+    if created:
+        chain = Chain()
+        chain.append(ocr_and_markdown_file, instance.id)
+        chain.append(rag_documentos, instance.id)
+        chain.run()

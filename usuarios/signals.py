9de280e344289ab7eb96e86867c20cc3549feb71
Álvaro Veiga <alvarojoao17@gmail.com>
2026-02-07 22:25:11 -0300

    Correção rapida no signals para poder enfileirar o processo de ocr via Qcluster

--- usuarios/signals.py
+++ usuarios/signals.py
@@ -2,8 +2,11 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import Documentos
 from ai.tasks import ocr_and_markdown_file, rag_documentos
+from django_q.tasks import async_task
 
 @receiver(post_save, sender=Documentos)
 def save_documents(sender, instance, created, **kwargs):
-    ocr_and_markdown_file(instance.id)
+    
+    async_task(ocr_and_markdown_file, instance.id)
+
     rag_documentos(instance.id)

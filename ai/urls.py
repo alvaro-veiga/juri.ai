95678fd222ca4cb4abe939f54d1f160b97bc3178
Álvaro Veiga <alvarojoao17@gmail.com>
2026-02-08 10:21:13 -0300

    Stream response para respostas do agente

--- ai/urls.py
+++ ai/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("chat/<int:id>", views.chat, name='chat'),
+    path("stream_response/", views.stream_response, name='stream_response'),
 ]
--- ai/views.py
+++ ai/views.py
@@ -2,6 +2,9 @@ from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
 from usuarios.models import Cliente
 from .models import Pergunta
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
+from .agent import JuriAI
 
 @csrf_exempt
 def chat(request, id):
@@ -13,3 +16,12 @@ def chat(request, id):
         pergunta_obj = Pergunta(pergunta=pergunta, cliente=cliente)
         pergunta_obj.save()
         return JsonResponse({'id': pergunta_obj.id, 'pergunta': pergunta_obj.pergunta})
+
+@csrf_exempt
+def stream_response(request):
+    id_pergunta = request.GET.get('id_pergunta')
+    pergunta = get_object_or_404(Pergunta, id=id_pergunta)
+
+    agente = JuriAI.build_agent()
+    resposta = agente.print_response(pergunta.pergunta)
+    return JsonResponse({'resposta': resposta})
